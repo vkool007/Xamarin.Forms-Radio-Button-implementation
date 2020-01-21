a8e61d2526a33d59d4a2095192f1dd4f9178f446
Vivek Kumar <48291812+vkool007@users.noreply.github.com>
2020-01-21 20:13:54 +0530

    Add files via upload
    These are only main solution file, I dont have uploaded Android and IOS code because there is no change in those folder . So what you have to do to create a new project as blank project and add the code in MainPage.xaml and cs file .

---
+++ RadioButtonDemo/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RadioButtonDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+
+        static string radioButtonType = "";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+         void OnNewRegClicked(object sender, EventArgs e)
+        {
+            if(radioButtonType=="")
+            {
+                DisplayAlert("Alert", "Please select Radio Button", "OK");
+            }
+            else
+                 if (radioButtonType == "abc")
+            {
+                DisplayAlert("Alert", "ABC is checked", "OK");
+            }
+            else
+                 if (radioButtonType == "xyz")
+            {
+                DisplayAlert("Alert", "XYZ is cheched", "OK");
+            }
+            
+
+              
+        }
+
+        void SMGClicked(object sender, EventArgs e)
+        {
+
+            SMGRbtn.BackgroundColor = Color.FromHex("#ff0000");
+            SMGRbtn.BorderColor = Color.FromHex("#ff0000");
+            VendorRbtn.BorderColor = Color.FromHex("#000000");
+            VendorRbtn.BackgroundColor = Color.FromHex("#ffffff");
+            radioButtonType = "abc";
+
+        }
+
+        void VendorClicked(object sender, EventArgs e)
+        {
+            SMGRbtn.BackgroundColor = Color.FromHex("#ffffff");
+            VendorRbtn.BorderColor = Color.FromHex("#ff0000");
+            SMGRbtn.BorderColor = Color.FromHex("#000000");
+            VendorRbtn.BackgroundColor = Color.FromHex("#ff0000");
+            radioButtonType = "xyz";
+        }
+    }
+}
